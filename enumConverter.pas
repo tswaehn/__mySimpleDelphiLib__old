56c57d37c13ccaf99cf2cd3b37d48c32de9aa5ce
Sven Ginka <sven.ginka@gmail.com>
2019-04-01 21:09:15 +0200

    mod: helper: string to enum converter

--- enumConverter.pas
+++ enumConverter.pas
@@ -4,10 +4,13 @@ interface
 
 uses classes, System.TypInfo;
 
+// https://stackoverflow.com/questions/2472487/converting-an-string-to-a-enum-type-using-tvalue
+
 type TEnumConverter = class
 public
   class function EnumToInt<T>(const EnumValue: T): Integer;
   class function EnumToString<T>(EnumValue: T): string;
+  class procedure StringToEnum<T>(strValue:String; var enValue:T);
 end;
 
 
@@ -22,6 +25,18 @@ end;
 class function TEnumConverter.EnumToString<T>(EnumValue: T): string;
 begin
   Result := GetEnumName(TypeInfo(T), EnumToInt(EnumValue));
+end;
+
+
+class procedure TEnumConverter.StringToEnum<T>(strValue: String; var enValue:T);
+   var Tipo : PTypeInfo;
+       Temp:Integer;
+       PTemp : pointer;
+begin
+    Tipo := TypeInfo(T);
+    Temp := GetEnumValue(Tipo, strValue);
+    PTemp := @Temp;
+    enValue := T(PTemp^);
 end;
 
 
