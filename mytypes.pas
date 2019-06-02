0b682851bb83ea86469e6030a9e6cf1b11ebeec7
Sven Ginka <sven.ginka@gmail.com>
2019-06-02 12:33:21 +0200

    add: int2bool

--- mytypes.pas
+++ mytypes.pas
@@ -6,6 +6,15 @@ uses Classes;
 type
   TStringListPtr = ^TStringList;
 
+
+  function IntToBool(const AnInt: Integer): Boolean;
+
 implementation
+
+function IntToBool(const AnInt: Integer): Boolean;
+begin
+   if AnInt = 0 then Result := False
+                else Result := True;
+end;
 
 end.
